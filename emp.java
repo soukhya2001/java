629420acf13ca3ede5cbce543bff44ca9813005e
soukhya2001 <78601416+soukhya2001@users.noreply.github.com>
2022-04-14 15:27:55 +0530

    Add files via upload

---
+++ emp.java
@@ -0,0 +1,16 @@
+package practicalday2;
+
+public class emp {
+	public static void main(String[] args)
+	{
+		int emp1=11000;
+		int emp2=5000;
+		int emp3=0;
+		   int total=10000*3;
+		  emp3=total-(emp1+emp2);
+		  System.out.println("The Third Employee ears"+emp3+"RS");
+		  
+		   
+	}
+
+}
