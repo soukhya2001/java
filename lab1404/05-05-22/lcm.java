a39456bfeacf6a5b89b000a2f0d0095523186c53
soukhya2001 <78601416+soukhya2001@users.noreply.github.com>
2022-05-05 15:52:59 +0530

    Add files via upload

---
+++ lab1404/05-05-22/lcm.java
@@ -0,0 +1,15 @@
+package Assign5;
+
+public class lcm {
+public static void main(String[] args)
+{
+	int a=3;
+	int b=4;
+	int x;
+	int l=240;
+	
+	x=240/(a*b);
+	x=x*a;
+	System.out.println("The smaller of two number is= "+x);
+}
+}
